9273fce9190ae75e0867d6ad82e8d239ae32b14f
Muhammad Raihan <muhammadraihan118@gmail.com>
2023-09-01 17:26:11 +0700

    remove console log

--- routes/index.js
+++ routes/index.js
@@ -27,7 +27,6 @@ router.get("/categories", async (req, res) => {
     const query = await pool.query("SELECT * FROM kategori");
     res.json({ body: query.rows });
   } catch (error) {
-    console.log(error);
     res.status(500).send(error.message);
   }
 });
